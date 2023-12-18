ce13d44d78a9666c7af1c93e7a4484437659654b
Jairaj <jairaju@mobiquityinc.com>
2023-12-18 15:31:46 +0530

    implemented api with database with user signup

---
+++ src/auth/users.repository.ts
@@ -0,0 +1,32 @@
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { User } from './user.entity';
+import { DataSource, Repository } from 'typeorm';
+
+@Injectable()
+export class UsersRepository extends Repository<User> {
+  constructor(private dataSource: DataSource) {
+    super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    try {
+      const { username, password } = authCredentialsDto;
+      const newUser = this.create({
+        username,
+        password,
+      });
+      await this.save(newUser);
+    } catch (err) {
+      if (err.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
