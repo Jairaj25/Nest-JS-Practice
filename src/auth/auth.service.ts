46c1c3af23ab30d771672dd0589389c2206441d7
Jairaj <jairaju@mobiquityinc.com>
2023-12-18 16:12:18 +0530

    added bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,26 @@
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private usersRepository: UsersRepository) {}
 
-  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<string> {
     return this.usersRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const foundUser = await this.usersRepository.findOneBy({
+      username: username,
+    });
+
+    if (foundUser && (await bcrypt.compare(password, foundUser.password))) {
+      return 'Successfully logged in';
+    } else {
+      throw new UnauthorizedException('Wrong login credentials');
+    }
   }
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -6,6 +6,7 @@ import {
 } from '@nestjs/common';
 import { User } from './user.entity';
 import { DataSource, Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersRepository extends Repository<User> {
@@ -13,14 +14,19 @@ export class UsersRepository extends Repository<User> {
     super(User, dataSource.createEntityManager());
   }
 
-  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<string> {
     try {
       const { username, password } = authCredentialsDto;
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(password, salt);
       const newUser = this.create({
         username,
-        password,
+        password: hashedPassword,
       });
-      await this.save(newUser);
+      const savedUser = await this.save(newUser);
+      if (savedUser) {
+        return 'User Created';
+      }
     } catch (err) {
       if (err.code === '23505') {
         throw new ConflictException('Username already exists');
